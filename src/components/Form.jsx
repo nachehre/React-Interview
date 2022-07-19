bd7b7e72b6e3f203a1e73ab01e9dd62b3e35a5b0
nachehre <zachehre@gmail.com>
2022-07-19 22:08:17 +0330

    Create Step Handler and Steps Component in Static Mode

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,27 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import { ColorlibConnector, ColorlibStepIcon } from './StepperStyle';
+import Step1 from './Step1';
+import Step2 from './Step2';
+import Step3 from './Step3';
+import Confirm from './Confirm';
 const FormStep =()=>{
     const steps = ['Step1', 'Step2', 'Step3', 'Step4'];
+    const handleSteps = (step) => {
+        switch (step) {
+          case 1:
+            return <Step1/>
+          case 2:
+            return <Step2/>
+          case 3:
+            return <Step3/>
+          case 4:
+                return <Confirm/>
+          default:
+            throw new Error('Unknown step')
+        }
+      }
+
     return (
         <Stack sx={{ width: '100%' }} spacing={4}>
   
@@ -15,6 +34,7 @@ const FormStep =()=>{
             </Step>
           ))}
         </Stepper>
+        {handleSteps(3)}
       </Stack>
     );
 }
